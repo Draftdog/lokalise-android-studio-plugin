ac1cb9812a147f315ad765bfe77184b37cef90f7
GitHub Action <action@github.com>
2022-03-19 16:25:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/draftdog/lokaliseandroidstudioplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.draftdog.lokaliseandroidstudioplugin.services
+
+import com.github.draftdog.lokaliseandroidstudioplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
